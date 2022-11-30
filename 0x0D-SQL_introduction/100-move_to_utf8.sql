72c5c3a97be79fd9fecfcecb36425e58882549e5
Timothy Kamau <kamautimothy635@gmail.com>
2022-11-30 06:25:38 +0000

    SQL_introduction

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -2,15 +2,15 @@
 -- as well as column `name` to utfmb4 encoding.
 USE hbtn_0c_0;
 ALTER DATABASE
-      hbtn_0c_0
-      CHARACTER SET = utf8mb4
-      COLLATE = utf8mb4_unicode_ci;
+	hbtn_0c_0
+        CHARACTER SET = utf8mb4
+        COLLATE = utf8mb4_unicode_ci;
 ALTER TABLE
-      first_table
-      CONVERT TO CHARACTER SET utf8mb4
-      COLLATE utf8mb4_unicode_ci;
+        first_table
+        CONVERT TO CHARACTER SET utf8mb4
+        COLLATE utf8mb4_unicode_ci;
 ALTER TABLE
-      first_table
-      CHANGE `name` `name` VARCHAR(256)
-      CHARACTER SET utf8mb4
-      COLLATE utf8mb4_unicode_ci;
+        first_table
+        CHANGE `name` `name` VARCHAR(256)
+        CHARACTER SET utf8mb4
+	COLLATE utf8mb4_unicode_ci;
