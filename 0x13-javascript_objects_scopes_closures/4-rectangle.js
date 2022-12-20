e648457bf82eaeccaf09726fb23b9561c80d0ab9
Timothy Kamau <kamautimothy635@gmail.com>
2022-12-20 07:21:49 +0000

    javascript_objects_scopes_closures

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,23 +1,27 @@
 #!/usr/bin/node
-module.expotrs = class Rectangle {
-  constructor (w, h) {
+module.expotrs = {
+  Rectangle: function (w, h) {
     if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     }
-  }
-  print () {
-    for (let i = 0; i < this.height; i++) {
-       console.log('X'.repeat(this.width));
+    this.print = function () {
+      let row = '';
+      for (let i = 0; i < this.width; i++) {
+	row = row.concat('X');
+      }
+      for (let i = 0; i < this.height; i++) {
+        console.log(row);
     }
-  }
-  rotate () {
-    let tmp = this.width;
-    this.width = this.height;
-    this.height = tmp;
-  }
-  double () {
+  };
+  this.rotate = function () {
+    let tmp = this.height;
+    this.height = this.width;
+    this.width = tmp;
+  };
+  this.double = function () {
     this.width *= 2;
     this.height *= 2;
+  };
   }
 };
