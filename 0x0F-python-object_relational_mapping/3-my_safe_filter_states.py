3767bffe09a530d3549a214f81b05f28d2fd445d
Timothy Kamau <kamautimothy635@gmail.com>
2022-12-30 12:31:45 +0000

    python-object_relational_mapping

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -12,10 +12,9 @@ if __name__ == "__main__":
                              database=sys.argv[3])
         cur = db.cursor()
         cur.execute("SELECT * FROM states "
-                    "WHERE name = %s "
-                    "COLLATE 'latin1_general_cs' "
-                    "ORDER BY id ASC", (sys.argv[4],))
+                    "ORDER BY id ASC")
         for row in cur.fetchall():
-            print(row)
+            if (state[1] == sys.arvg[4]):
+                print(row)
         cur.close()
         db.close()
