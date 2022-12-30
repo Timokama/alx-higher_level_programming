825e408592d2ae8a72df16c63ffbd627ce25743e
Timothy Kamau <kamautimothy635@gmail.com>
2022-12-30 11:30:35 +0000

    python-object_relational_mapping

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+from sys import argv
+import MySQLdb
+
+if __name__ == '__main__':
+    the_usa = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    commands = the_usa.cursor()
+    commands.execute("SELECT cities.id, cities.name, states.name FROM\
+    cities JOIN states ON cities.state_id = states.id\
+    ORDER BY cities.id ASC")
+    usa = command.fetchall()
+    comma_print = 0
+    for state in usa:
+        if state[2] == argv[4]:
+            if comma_print > 0:
+                print(", ", end="")
+            print("{:s}".format(state[1]), end="")
+            comma_print +=1
+    print("")
+    command.close()
+    the_usa.close()
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # alx-higher_level_programming
-Practicing and applying this new knowledge while I'm studying at Alx School. This repo contains scripts in python.
+Practicing and applying this new knowledge while I'm studying at Alx School. This repo contains scripts in python and javascript.
