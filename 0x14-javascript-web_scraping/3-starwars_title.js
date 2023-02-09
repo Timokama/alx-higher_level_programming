ae4086bd47418203ff8b9162463015eae009b946
Timothy Kamau <kamautimothy635@gmail.com>
2023-02-09 11:52:57 +0000

    starwars title

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const request = require('request');
 request.get('https://swapi-api.alx-tools.com/api/films/:id' + process.argv[2] + '/', (err, response, body) => {
-	if (err) console.log(err);
-	else if (response.statusCode === 200) {
-	  console.log(JSON.parse(body).title);
-	}
+  if (err) console.log(err);
+  else if (response.statusCode === 200) {
+    console.log(JSON.parse(body).title);
+  }
 });
