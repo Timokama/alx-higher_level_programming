50a391c12916227fd320cdb4af500e8bb31c56d8
Timothy Kamau <kamautimothy635@gmail.com>
2023-02-09 11:41:29 +0000

    starwars count

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,16 +1,16 @@
 #!/usr/bin/node
 const request = require('request');
 request.get(process.argv[2], (err, response, bode) => {
-	if (err) console.log(err);
-	else if (response.statusCode === 200) {
-	  let films = JSON.parse(body).results;
-	  let num = 0;
-	  for (let film of films) {
-	    for (let ch of film.characters) {
-	      if (ch.endsWith('18/')) { num++; break; }
-	    }
-	  }
-	  console.log(num);
-	}
+  if (err) console.log(err);
+  else if (response.statusCode === 200) {
+    let films = JSON.parse(body).results;
+    let num = 0;
+    for (let film of films) {
+      for (let ch of film.characters) {
+        if (ch.endsWith('18/')) { num++; break; }
+      }
+    }
+    console.log(num);
+  }
 });
 			
