8c36baf7281103621bab19062f1a375357315c28
Timothy Kamau <kamautimothy635@gmail.com>
2023-02-09 10:10:58 +0000

    Write file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-fs.writeFile(process.argv[2], process.argv[3], 'utf8', (err) => {
+fs.writeFile(process.argv[2], process.argv[3], 'utf-8', (err) => {
 	if (err) console.log(err);
 });
 
